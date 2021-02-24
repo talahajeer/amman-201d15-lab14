3b1ced5a65f268104137f3c1b59fb697c97c73e9
samer <s.allaham@ltuc.com>
2021-02-24 11:12:01 +0200

    removing css

--- js/app.js
+++ js/app.js
@@ -8,10 +8,14 @@ const Cart = function (items) {
 
 Cart.prototype.addItem = function (product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+   let newItem  = new CartItem(product,quantity);
+   this.items.push(newItem);
+
 };
 
 Cart.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('cart',JSON.stringify(this.items))
 };
 
 Cart.prototype.removeItem = function (item) {
